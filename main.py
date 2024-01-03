c2806c090c3335146a72208dd9bba2f42700f987
pro <storchakvasil@gmail.com>
2024-01-03 20:40:52 +0300

    Add AI module as submodule + GPU

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from PySide6.QtCore import QFile
 
 # main function
 if __name__ == "__main__":
+    # load style from file
     style_sheet = QFile('src/resource/Yashi.qss')
     style_sheet.open(QFile.OpenModeFlag.ReadOnly)
     convert = style_sheet.readAll().toStdString()
 
     app = QApplication(sys.argv)
+    # load style to App
     app.setStyleSheet(convert)
 
     widget = Widget()
